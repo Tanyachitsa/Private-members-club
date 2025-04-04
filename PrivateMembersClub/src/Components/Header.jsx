0502f4a7d0785476b43af786356ae3a462850203
Tanyachitsa <165687341+Tanyachitsa@users.noreply.github.com>
2025-04-05 00:13:34 +0100

    Added header component

--- PrivateMembersClub/src/Components/Header.jsx
+++ PrivateMembersClub/src/Components/Header.jsx
@@ -1,3 +1,20 @@
 export default function Header() {
-  return <></>;
+  return (
+    <>
+      <nav>
+        <img
+          className="logo"
+          src="/src/Media/AlexanderHouse.png"
+          alt="Alexander House logo"
+        />
+        <ul>
+          <li>Houses</li>
+          <li>Wellness</li>
+          <li>What's On</li>
+          <li>Membership</li>
+        </ul>
+        <span className="sign-in">Sign in</span>
+      </nav>
+    </>
+  );
 }
