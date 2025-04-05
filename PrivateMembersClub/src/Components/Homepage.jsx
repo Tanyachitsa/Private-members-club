9d766914682a1b8137a3126fd889a7b002896921
Tanyachitsa <165687341+Tanyachitsa@users.noreply.github.com>
2025-04-05 13:15:15 +0100

    added the homepage summary

--- PrivateMembersClub/src/Components/Homepage.jsx
+++ PrivateMembersClub/src/Components/Homepage.jsx
@@ -9,6 +9,18 @@ export default function Homepage() {
         loop
         muted
       ></video>
+      <br />
+      <p className="homepage-summary">
+        <strong className="homepage-summary-title">
+          Alexander House is a refined sanctuary for modern professionals,
+          creatives, and discerning minds.
+        </strong>
+        Designed for those who value privacy, connection, and elevated living,
+        membership unlocks access to elegant lounges, curated dining, cultural
+        salons, wellness spaces, and luxurious overnight stays. With limited
+        memberships, tailored events, and a focus on community over crowd,
+        Alexander House is more than a club — it's your second home.
+      </p>
     </section>
   );
 }
