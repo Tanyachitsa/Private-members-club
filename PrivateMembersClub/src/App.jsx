f0c48d9fd0188b338258701d85b423663341cc0c
Tanyachitsa <165687341+Tanyachitsa@users.noreply.github.com>
2025-04-05 14:19:51 +0100

    adding more information and images to the homepage

--- PrivateMembersClub/src/App.jsx
+++ PrivateMembersClub/src/App.jsx
@@ -1,11 +1,13 @@
 import Header from "./Components/Header";
 import Homepage from "./Components/Homepage";
+import Footer from "./Components/Footer";
 
 export default function App() {
   return (
     <>
       <Header />
       <Homepage />
+      <Footer />
     </>
   );
 }
