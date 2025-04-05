061ab69c55205ec5c2328e28e5ee1b57709beeaf
Tanyachitsa <165687341+Tanyachitsa@users.noreply.github.com>
2025-04-05 14:59:00 +0100

    Added more to the footer component

--- PrivateMembersClub/src/Components/Footer.jsx
+++ PrivateMembersClub/src/Components/Footer.jsx
@@ -1,3 +1,27 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faInstagram,
+  faLinkedin,
+  faTwitter,
+} from "@fortawesome/free-brands-svg-icons";
+
 export default function Footer() {
-  return <h1></h1>;
+  return (
+    <>
+      <p>
+        Alexander House
+        <br />
+        10 Hanover Square Mayfair, London
+        <br />
+        W1S 1DA United Kingdom
+        <br />
+        020 7946 1234
+      </p>
+      <div className="icon-container">
+        <FontAwesomeIcon icon={faInstagram} />
+        <FontAwesomeIcon icon={faLinkedin} />
+        <FontAwesomeIcon icon={faTwitter} />
+      </div>
+    </>
+  );
 }
