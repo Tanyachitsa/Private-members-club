a1417242126f4ec89d4863fa8847a69bb67cbaa9
Tanyachitsa <165687341+Tanyachitsa@users.noreply.github.com>
2025-04-08 15:29:35 +0100

    Linked my files to the navbar

--- PrivateMembersClub/src/App.jsx
+++ PrivateMembersClub/src/App.jsx
@@ -1,13 +1,22 @@
-import Header from "./Components/Header";
-import Homepage from "./Components/Homepage";
-import Footer from "./Components/Footer";
+import { Routes, Route } from "react-router-dom";
+import Homepage from "./Pages/Homepage";
+import Events from "./Pages/Events";
+import Houses from "./Pages/Houses";
+import Membership from "./Pages/Membership";
+import SignIn from "./Pages/SignIn";
+import Wellness from "./Pages/Wellness";
 
 export default function App() {
   return (
     <>
-      <Header />
-      <Homepage />
-      <Footer />
+      <Routes>
+        <Route path="/" element={<Homepage />} />
+        <Route path="/Houses" element={<Houses />} />
+        <Route path="/Events" element={<Events />} />
+        <Route path="/Wellness" element={<Wellness />} />
+        <Route path="/Membership" element={<Membership />} />
+        <Route path="/SignIn" element={<SignIn />} />
+      </Routes>
     </>
   );
 }
--- PrivateMembersClub/src/Components/Header.jsx
+++ PrivateMembersClub/src/Components/Header.jsx
@@ -1,20 +1,31 @@
+import { Link } from "react-router-dom";
 export default function Header() {
   return (
-    <>
-      <nav>
+    <nav>
+      <Link to="/">
         <img
           className="logo"
           src="/src/Media/AlexanderHouse.png"
           alt="Alexander House logo"
         />
-        <ul>
+      </Link>
+      <ul>
+        <Link to="/Houses">
           <li>Houses</li>
+        </Link>
+        <Link to="/Wellness">
           <li>Wellness</li>
+        </Link>
+        <Link to="/Events">
           <li>What's On</li>
+        </Link>
+        <Link to="/Membership">
           <li>Membership</li>
-        </ul>
+        </Link>
+      </ul>
+      <Link to="/SignIn">
         <span className="sign-in">Sign in</span>
-      </nav>
-    </>
+      </Link>
+    </nav>
   );
 }
