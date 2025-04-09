b6b4f5864bff481989a2ba21ba7f7f211acb54dd
Tanyachitsa <165687341+Tanyachitsa@users.noreply.github.com>
2025-04-09 13:57:06 +0100

    Created the Houses jsx file

---
+++ PrivateMembersClub/src/Components/HouseImages.jsx
@@ -0,0 +1,171 @@
+export default function HouseImages() {
+  return (
+    <>
+      <div class="container text-center">
+        <div class="row">
+          <div class="col">
+            <img
+              src="/src/Media/houseMedia/stlucia.jpg"
+              alt="image of hotel reception"
+              className="house-img"
+            />
+            <br />
+            <h4>The Palm Pavilion </h4>
+            <h5>- St Lucia -</h5>
+            <p>
+              A tranquil escape surrounded by lush greenery and natural wood
+              finishes.
+            </p>
+          </div>
+          <div class="col">
+            <img
+              src="/src/Media/houseMedia/jordan.jpg"
+              alt="image of hotel reception"
+              className="house-img"
+            />
+            <br />
+            <h4>The Medina Room </h4>
+            <h5>- Jordan -</h5>
+            <p>Tiled elegance and intimate courtyards create a hidden haven.</p>
+          </div>
+          <div class="col">
+            <img
+              src="/src/Media/houseMedia/london.jpg"
+              alt="image of hotel reception"
+              className="house-img"
+            />
+            <br />
+            <h4>The Guilded Room </h4>
+            <h5>- London -</h5>
+            <p>
+              Golden accents and plush finishes evoke timeless elegance and
+              classic comfort.
+            </p>
+          </div>
+        </div>
+      </div>
+
+      <div class="container text-center">
+        <div class="row">
+          <div class="col">
+            <img
+              src="/src/Media/houseMedia/manchester.jpg"
+              alt="image of hotel reception"
+              className="house-img"
+            />
+            <br />
+            <h4>The Loft </h4>
+            <h5>- Manchester -</h5>
+            <p>
+              An airy, modern hideaway with industrial charm and cityscape
+              skylines.
+            </p>
+          </div>
+
+          <div class="col">
+            <img
+              src="/src/Media/houseMedia/morocco.jpg"
+              alt="image of hotel reception"
+              className="house-img"
+            />
+            <br />
+            <h4>The Marrakesh Terrace </h4>
+            <h5>- Morocco -</h5>
+            <p>Vibrant mosaics and lantern-lit ambiance.</p>
+          </div>
+          <div class="col">
+            <img
+              src="/src/Media/houseMedia/paris.jpg"
+              alt="image of hotel reception"
+              className="house-img"
+            />
+            <br />
+            <h4>The Artsian Studio </h4>
+            <h5>- Paris -</h5>
+            <p>
+              Textured walls, earthy palettes, and vintage furniture offer
+              handcrafted comfort.
+            </p>
+          </div>
+        </div>
+      </div>
+
+      <div class="container text-center">
+        <div class="row">
+          <div class="col">
+            <img
+              src="/src/Media/houseMedia/lakecomo.jpg"
+              alt="image of hotel reception"
+              className="house-img"
+            />
+            <br />
+            <h4>The Roman Garden Suite </h4>
+            <h5>- Italy -</h5>
+            <p>Classical details meet lush greenery in timeless style.</p>
+          </div>
+
+          <div class="col">
+            <img
+              src="/src/Media/houseMedia/swissalps.jpg"
+              alt="image of hotel reception"
+              className="house-img"
+            />
+            <br />
+            <h4>The Glacier Loft </h4>
+            <h5>- Switzerland -</h5>
+            <p>Mountain luxury with plush linens and fur throws.</p>
+          </div>
+          <div class="col">
+            <img
+              src="/src/Media/houseMedia/zimbabwe.jpeg"
+              alt="image of hotel reception"
+              className="house-img"
+            />
+            <br />
+            <h4>The Baobab Suite </h4>
+            <h5>- Zimbabwe -</h5>
+            <p>Rooted in strength and beauty, with grounding, natural decor.</p>
+          </div>
+        </div>
+      </div>
+
+      <div class="container text-center">
+        <div class="row">
+          <div class="col">
+            <img
+              src="/src/Media/houseMedia/egypt.jpg"
+              alt="image of hotel reception"
+              className="house-img"
+            />
+            <br />
+            <h4>The Oasis Pavilion </h4>
+            <h5>- Egypt -</h5>
+            <p>Fragrant, peaceful, and filled with quiet romance.</p>
+          </div>
+          <div class="col">
+            <img
+              src="/src/Media/houseMedia/mauritius.jpg"
+              alt="image of hotel reception"
+              className="house-img"
+            />
+            <br />
+            <h4>The Hibiscus Hideaway </h4>
+            <h5>- Mauritius -</h5>
+            <p>Vibrant, floral, and intimate — a bold yet calming retreat.</p>
+          </div>
+          <div class="col">
+            <img
+              src="/src/Media/houseMedia/tuscany.jpg"
+              alt="image of hotel reception"
+              className="house-img"
+            />
+            <br />
+            <h4>The Tuscan Retreat </h4>
+            <h5>- Italy -</h5>
+            <p>Warm stone walls, rustic charm, and golden light..</p>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
--- PrivateMembersClub/src/Pages/Houses.jsx
+++ PrivateMembersClub/src/Pages/Houses.jsx
@@ -1,3 +1,24 @@
+import Header from "../Components/Header";
+import Footer from "../Components/Footer";
+import HouseImages from "../Components/HouseImages";
+
 export default function Houses() {
-  return <h1>Hello houses</h1>;
+  return (
+    <>
+      <Header />
+      <h1>Houses</h1>
+      <p>
+        Experience <strong>timeless luxury</strong> with our curated overnight
+        stays at Alexander House. Each suite blends elegant design with the
+        comfort of home — featuring plush furnishings, private amenities, and
+        thoughtful touches throughout. Whether you're seeking a quiet retreat, a
+        romantic escape, or a refined base for your city visit, our
+        accommodations offer the perfect balance of privacy, warmth, and
+        sophistication.
+      </p>
+
+      <HouseImages />
+      <Footer />
+    </>
+  );
 }
