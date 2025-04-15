70ca14ea2765fee33e1931cdac20644d54c1b92f
Tanyachitsa <165687341+Tanyachitsa@users.noreply.github.com>
2025-04-15 23:02:23 +0100

    updated font awesome download

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,13 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import commonjs from "vite-plugin-commonjs";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [react(), commonjs()],
+  build: {
+    rollupOptions: {
+      external: ["@fortawesome/free-brands-svg-icons"],
+    },
+  },
+});
